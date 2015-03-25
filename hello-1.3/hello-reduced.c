1cb058e13176e80548c8cc2029d205fcef83e55f
youshu.liu <youshu.liu@ae679346-658d-4836-96a3-7851ac642e8f>
2015-03-25 14:16:32 +0000

    sample initialize

---
+++ hello-1.3/hello-reduced.c
@@ -0,0 +1,36 @@
+﻿/* hello -- print a greeting message and exit. */
+
+/* AIX requires this to be the first thing in the file.  */
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <errno.h>
+#include "getopt.h"
+static char *progname;
+
+int
+main (argc, argv)
+     int argc;
+     char *argv[];
+{
+  int optc;
+  int h = 0, v = 0, t = 0, m = 0, lose = 0, z = 0;
+
+  progname = argv[0];
+
+}
+/* Used by alloca.c.  */
+
+char *
+xmalloc (size)
+     unsigned int size;
+{
+  char *ptr = malloc (size);
+  if (! ptr)
+    {
+      fprintf (stderr, "%s: virtual memory exhausted\n", progname);
+      exit (1);
+    }
+  return ptr;
+}
