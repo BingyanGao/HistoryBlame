2172e225c8e2007ed02dfc69108971f5f2b46819
youshu.liu <youshu.liu@ae679346-658d-4836-96a3-7851ac642e8f>
2015-03-25 17:23:46 +0000

    delete a  line after the modified line

--- hello-1.3/hello-reduced.c
+++ hello-1.3/hello-reduced.c
@@ -1,5 +1,4 @@
-﻿/* hello -- print a greeting message and exit. */
-/* AIX requires this to be the first thing in the file!!!  */
+﻿/* hello -- print a greeting message and exit. !!!*/
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
